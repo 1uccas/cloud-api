34695e4c546e8ab4294db18f41dfcefe95980456
luccas <nascimentolucas2164@gmail.com>
2024-10-23 18:11:55 -0300

    feat require dotenv

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php 
+require 'vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
+$dotenv->load();
 
 $url = "https://api.hgbrasil.com/weather";
 $api = json_decode(file_get_contents($url));
