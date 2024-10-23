c7f69021c7062de752fe23ae18712a6efc3dc3ef
luccas <nascimentolucas2164@gmail.com>
2024-10-23 18:37:45 -0300

    feat insert Fortaleza city code in API url

--- index.php
+++ index.php
@@ -4,8 +4,7 @@ $dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
 $dotenv->load();
 
 $API_KEY = getenv('API_KEY');
-
-$url = "https://api.hgbrasil.com/weather?key=".$API_KEY;
+$url = "https://api.hgbrasil.com/weather?woeid=455830?key=".$API_KEY;
 $api = json_decode(file_get_contents($url));
 
 var_dump($api);
