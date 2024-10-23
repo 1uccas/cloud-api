69ccb5388beb3a3b9eb6bde1e3584bf6bfbe1719
luccas <nascimentolucas2164@gmail.com>
2024-10-23 18:14:48 -0300

    feat use API key from environment variable

--- index.php
+++ index.php
@@ -3,7 +3,9 @@ require 'vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
 $dotenv->load();
 
-$url = "https://api.hgbrasil.com/weather";
+$API_KEY = getenv('API_KEY');
+
+$url = "https://api.hgbrasil.com/weather?key=".$API_KEY;
 $api = json_decode(file_get_contents($url));
 
 var_dump($api);
