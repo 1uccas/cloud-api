cf04f4871293a0be70f783113e911cc1bca82fea
luccas <nascimentolucas2164@gmail.com>
2024-11-20 17:16:05 -0300

    feat remove dotenv, getenv and req API

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
 <?php 
-require 'vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
-$dotenv->load();
-
-$API_KEY = getenv('API_KEY');
-$url = "https://api.hgbrasil.com/weather?woeid=455830?key=".$API_KEY;
-$api = json_decode(file_get_contents($url));
-
 require 'src/show_api.php';
 ?>
