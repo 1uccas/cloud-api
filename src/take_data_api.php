91cf2a81b9f89b46ba11b854e1a32b006635f36d
luccas <nascimentolucas2164@gmail.com>
2024-11-20 17:19:32 -0300

    feat remove data from support api

--- src/take_data_api.php
+++ src/take_data_api.php
@@ -1,40 +1,4 @@
 <?php 
-/*
-//full results in API
-$results = $api->results;
-
-$results_tomorrow = $results->forecast[1]; //FULL RESULTS OF TOMORROW!
-
-$date = $results->date;
-$hour_time = $results->time;
-$condition_description = $results->description;
-$currently = $results->currently;
-$humidity = $results->humidity;
-$city = $results->city;
-$cloudiness = $results->cloudiness;
-$rain = $results->rain;
-$wind_speedy = $results->wind_speedy;
-$wind_direction = $results->wind_direction;
-$wind_cardinal = $results->wind_cardinal;
-$sunrise = $results->sunrise;
-$sunset = $results->sunset;
-
-#echo $results_tomorrow->description;
-
-$moon = $results->moon_phase;
-$condition_slug = $results->condition_slug;
-
-$req_monn = $moon;
-$req_condition_slug = $condition_description;
-
-$picture_monn = "https://assets.hgbrasil.com/weather/icons/moon/$moon.png";
-$picture_condition_slug = "https://assets.hgbrasil.com/weather/icons/conditions/$condition_slug.svg";
-
-//print_r($results)
-*/
-?>
-
-<?php 
 
 class API{
 	//Criação de variáveis.
